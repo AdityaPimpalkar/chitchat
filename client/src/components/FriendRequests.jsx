3e9a2ed1abe5d0e6f1d62acf257cb7f29fbf9e13
Aditya Pimpalkar <adipimpalkar@gmail.com>
2021-12-29 23:47:12 +0530

    icons replacement

--- client/src/components/FriendRequests.jsx
+++ client/src/components/FriendRequests.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { BadgeCheckIcon, CheckIcon } from "@heroicons/react/solid";
 const FriendRequests = ({ friendRequests, selectFriend, acceptRequest }) => {
   return (
     <div className="flex flex-row overflow-y-auto w-full">
@@ -26,18 +26,7 @@ const FriendRequests = ({ friendRequests, selectFriend, acceptRequest }) => {
                         {friend.username}
                       </span>
                       <div className="flex justify-start items-center">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          className="text-blue-400 lg:h-3 lg:w-3 xl:h-5 xl:w-5 2xl:h-6 2xl:w-6"
-                          viewBox="0 0 20 20"
-                          fill="currentColor"
-                        >
-                          <path
-                            fill-rule="evenodd"
-                            d="M6.267 3.455a3.066 3.066 0 001.745-.723 3.066 3.066 0 013.976 0 3.066 3.066 0 001.745.723 3.066 3.066 0 012.812 2.812c.051.643.304 1.254.723 1.745a3.066 3.066 0 010 3.976 3.066 3.066 0 00-.723 1.745 3.066 3.066 0 01-2.812 2.812 3.066 3.066 0 00-1.745.723 3.066 3.066 0 01-3.976 0 3.066 3.066 0 00-1.745-.723 3.066 3.066 0 01-2.812-2.812 3.066 3.066 0 00-.723-1.745 3.066 3.066 0 010-3.976 3.066 3.066 0 00.723-1.745 3.066 3.066 0 012.812-2.812zm7.44 5.252a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                            clip-rule="evenodd"
-                          />
-                        </svg>
+                        <BadgeCheckIcon className="text-blue-400 lg:h-3 lg:w-3 xl:h-5 xl:w-5 2xl:h-6 2xl:w-6" />
                         <span className="text-green-300 truncate lg:text-xs 2xl:text-base">
                           Verified
                         </span>
@@ -45,18 +34,7 @@ const FriendRequests = ({ friendRequests, selectFriend, acceptRequest }) => {
                     </div>
                     <div className="flex flex-col items-center border-b border-purple-400 flex-1">
                       <button onClick={() => acceptRequest(friend)}>
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          className="lg:h-5 lg:w-5 xl:h-8 xl:w-8 2xl:h-11 2xl:w-11"
-                          viewBox="0 0 20 20"
-                          fill="currentColor"
-                        >
-                          <path
-                            fill-rule="evenodd"
-                            d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                            clip-rule="evenodd"
-                          />
-                        </svg>
+                        <CheckIcon className="lg:h-5 lg:w-5 xl:h-8 xl:w-8 2xl:h-11 2xl:w-11" />
                       </button>
                     </div>
                   </div>
