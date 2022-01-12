7fd8167e3bf7750d4939a9e3c15c0d41cb35e742
Aditya Pimpalkar <adipimpalkar@gmail.com>
2022-01-12 22:28:30 +0530

    is loading check

--- client/src/screens/Login.jsx
+++ client/src/screens/Login.jsx
@@ -20,26 +20,26 @@ const Login = ({ onSuccess, onFailure, isLoading, LoadWidth }) => {
           </div>
           <h2 className="text-3xl text-center text-white mb-4">chitchat.io</h2>
           <div className="px-8 pb-10">
-            <GoogleLogin
-              clientId={config.clientId}
-              render={(renderProps) => (
-                <div className="w-full ">
-                  <button
-                    className={` w-full flex flex-row px-10 py-3 justify-center items-center rounded-full ${
-                      isLoading === false &&
-                      "hover:shadow-xl hover:bg-yellow-500 transition duration-150 ease-in-out"
-                    }`}
-                    onClick={isLoading === false ? renderProps.onClick : null}
-                    disabled={renderProps.disabled}
-                  >
-                    {isLoading && (
-                      <div className="w-full flex bg-purple-900 justify-start items-center bg-gray-200 h-6">
-                        <div
-                          className={`bg-yellow-600 h-2 transition 2s ${LoadWidth}`}
-                        ></div>
-                      </div>
-                    )}
-                    {!isLoading && (
+            {isLoading === true && (
+              <div className="w-full flex bg-purple-900 justify-start items-center bg-gray-200 h-6">
+                <div
+                  className={`bg-yellow-600 h-2 transition 2s ${LoadWidth}`}
+                ></div>
+              </div>
+            )}
+            {isLoading === false && (
+              <GoogleLogin
+                clientId={config.clientId}
+                render={(renderProps) => (
+                  <div className="w-full ">
+                    <button
+                      className={` w-full flex flex-row px-10 py-3 justify-center items-center rounded-full ${
+                        isLoading === false &&
+                        "hover:shadow-xl hover:bg-yellow-500 transition duration-150 ease-in-out"
+                      }`}
+                      onClick={isLoading === false ? renderProps.onClick : null}
+                      disabled={renderProps.disabled}
+                    >
                       <React.Fragment>
                         <img
                           src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
@@ -52,16 +52,16 @@ const Login = ({ onSuccess, onFailure, isLoading, LoadWidth }) => {
                           </span>
                         </div>
                       </React.Fragment>
-                    )}
-                  </button>
-                </div>
-              )}
-              buttonText="Login"
-              onSuccess={onSuccess}
-              onFailure={onFailure}
-              cookiePolicy={"single_host_origin"}
-              isSignedIn={true}
-            />
+                    </button>
+                  </div>
+                )}
+                buttonText="Login"
+                onSuccess={onSuccess}
+                onFailure={onFailure}
+                cookiePolicy={"single_host_origin"}
+                isSignedIn={true}
+              />
+            )}
           </div>
         </form>
       </div>
