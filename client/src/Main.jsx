21655c0b297b1e75977a34c962c5142845415d4b
Aditya Pimpalkar <adipimpalkar@gmail.com>
2022-02-07 23:20:57 +0530

    main component restructure

--- client/src/Main.jsx
+++ client/src/Main.jsx
@@ -1,103 +1,100 @@
-import React, { Component } from "react";
+import React, { Component, useEffect, useState } from "react";
 import socket from "./www/socket";
 import Login from "./screens/Login";
 import Chat from "./screens/Chat";
 import Chats from "./screens/Chats";
-class Main extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      socket,
-      user: "",
-      loggedInUser: {},
-      users: [],
-      groups: [],
-      friendRequests: [],
-      isLoading: true,
-      LoadWidth: "w-32",
-      isConnected: false,
-    };
-  }
+import SocketEvents from "./events/constants";
 
-  componentDidMount() {
-    this.checkIfUserExists();
+const Main = () => {
+  const [loggedInUser, setLoggedInUser] = useState({});
+  const [users, setUsers] = useState([]);
+  const [groups, setGroups] = useState([]);
+  const [friendRequests, setFriendRequests] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadWidth, setLoadWidth] = useState("w-32");
+  const [isConnected, setIsConnected] = useState(false);
 
-    const { socket } = this.state;
-
-    socket.on("connect", () => {
-      this.setState({ isConnected: true });
-    });
-
-    socket.on("disconnect", () => {
-      this.setState({ isConnected: false });
-    });
-
-    socket.on("session", ({ token, userId, username, image }) => {
-      socket.auth = { token };
-      localStorage.setItem("token", token);
-      const loggedInUser = { userId, username, image };
-      this.setState({ loggedInUser, LoadWidth: "w-96", isLoading: false });
-    });
-
-    socket.on("users", ({ users, groups, friendRequests }) => {
-      this.setState({ users, groups, friendRequests, LoadWidth: "w-64" });
-    });
-  }
-
-  checkIfUserExists = () => {
-    const { socket } = this.state;
+  const userExists = () => {
     const token = localStorage.getItem("token");
     if (token) {
       socket.auth = { token };
       socket.connect();
     } else {
-      this.setState({ LoadWidth: "w-96", isLoading: false });
+      setLoadWidth("w-96");
+      setIsLoading(false);
     }
   };
 
-  onLoginSuccess = (response) => {
+  useEffect(() => {
+    userExists();
+
+    socket.on(SocketEvents.CONNECT, () => {
+      setIsConnected(true);
+    });
+
+    socket.on(SocketEvents.DISCONNECT, () => {
+      setIsConnected(false);
+    });
+
+    socket.on(SocketEvents.CONNECT_ERROR, function (err) {
+      setIsConnected(false);
+    });
+
+    socket.on(SocketEvents.SESSION, ({ token, userId, username, image }) => {
+      socket.auth = { token };
+      localStorage.setItem("token", token);
+      setLoadWidth("w-96");
+      setIsLoading(false);
+      setLoggedInUser({ userId, username, image });
+    });
+
+    socket.on(SocketEvents.USERS, ({ users, groups, friendRequests }) => {
+      setUsers(users);
+      setGroups(groups);
+      setFriendRequests(friendRequests);
+      setLoadWidth("w-64");
+    });
+
+    return () => {
+      socket.off(SocketEvents.CONNECT);
+      socket.off(SocketEvents.DISCONNECT);
+      socket.off(SocketEvents.CONNECT_ERROR);
+      socket.off(SocketEvents.SESSION);
+      socket.off(SocketEvents.USERS);
+    };
+  }, []);
+
+  const onLoginSuccess = (response) => {
     const { profileObj, tokenId } = response;
-    const { socket } = this.state;
     socket.auth = { user: profileObj, tokenId };
     socket.connect();
   };
 
-  onLoginFailure = () => {
+  const onLoginFailure = () => {
     console.log("Login Failed!");
   };
 
-  render() {
-    const {
-      users,
-      groups,
-      friendRequests,
-      loggedInUser,
-      isLoading,
-      LoadWidth,
-      isConnected,
-    } = this.state;
-    return (
-      <main>
-        {loggedInUser.userId && (
-          <Chats
-            user={loggedInUser}
-            users={users}
-            groups={groups}
-            friendRequests={friendRequests}
-            isConnected={isConnected}
-          />
-        )}
-        {!loggedInUser.userId && (
-          <Login
-            onSuccess={this.onLoginSuccess}
-            onFailure={this.onLoginFailure}
-            isLoading={isLoading}
-            LoadWidth={LoadWidth}
-          />
-        )}
-      </main>
-    );
-  }
-}
+  return (
+    <main>
+      {loggedInUser.userId && (
+        <Chats
+          user={loggedInUser}
+          users={users}
+          groups={groups}
+          friendRequests={friendRequests}
+          isConnected={isConnected}
+        />
+      )}
+      {!loggedInUser.userId && (
+        <Login
+          onSuccess={(response) => onLoginSuccess(response)}
+          onFailure={() => onLoginFailure()}
+          isLoading={isLoading}
+          LoadWidth={loadWidth}
+        />
+      )}
+    </main>
+  );
+};
 
 export default Main;
---
+++ server/src/config/socketEvents.js
@@ -0,0 +1,5 @@
+const socketEvents = {
+  USER_CONNECTED: "user connected",
+  USER_DISCONNECTED: "user disconnected",
+  USER_MESSAGES: "user messages",
+};
