c7d4380f520d29e2d163382f671a8259faa62466
Aditya Pimpalkar <adipimpalkar@gmail.com>
2022-02-10 23:57:49 +0530

    renaming constants

--- client/src/events/constants.js
+++ client/src/events/constants.js
@@ -2,11 +2,12 @@ const SocketEvents = {
   CONNECT: "connect",
   DISCONNECT: "disconnect",
   CONNECT_ERROR: "connect_error",
+  SERVER_ERROR: "server_error",
   SESSION: "session",
-  USERS: "users",
-  USER_CONNECTED: "user connected",
-  USER_DISCONNECTED: "user disconnected",
-  PRIVATE_MESSAGE: "private message",
-  USER_MESSAGES: "user messages",
+  LOAD_DATA: "load_data",
+  USER_CONNECTED: "user_connected",
+  USER_DISCONNECTED: "user_disconnected",
+  PRIVATE_MESSAGE: "private_message",
+  USER_MESSAGES: "user_messages",
 };
 export default SocketEvents;
--- server/src/config/socketEvents.js
+++ server/src/config/socketEvents.js
@@ -1,5 +1,13 @@
 const socketEvents = {
-  USER_CONNECTED: "user connected",
-  USER_DISCONNECTED: "user disconnected",
-  USER_MESSAGES: "user messages",
+  CONNECT: "connect",
+  DISCONNECT: "disconnect",
+  CONNECT_ERROR: "connect_error",
+  SERVER_ERROR: "server_error",
+  SESSION: "session",
+  LOAD_DATA: "load_data",
+  USER_CONNECTED: "user_connected",
+  USER_DISCONNECTED: "user_disconnected",
+  PRIVATE_MESSAGE: "private_message",
+  USER_MESSAGES: "user_messages",
 };
+export default socketEvents;
