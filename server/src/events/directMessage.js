4d0705916009c14a58639ad07701d467625e8cd5
Aditya Pimpalkar <adipimpalkar@gmail.com>
2022-02-12 23:30:39 +0530

    try catch block

--- server/src/events/directMessage.js
+++ server/src/events/directMessage.js
@@ -3,9 +3,13 @@ import socketEvents from "../config/socketEvents.js";
 import { privateMessages, userMessages } from "../tasks/directMessage.js";
 
 export default function directMessageEvents() {
-  socket.on(socketEvents.PRIVATE_MESSAGE, (message) =>
-    privateMessages(message)
-  );
+  try {
+    socket.on(socketEvents.USER_MESSAGES, (user) => userMessages(user));
 
-  socket.on(socketEvents.USER_MESSAGES, (user) => userMessages(user));
+    socket.on(socketEvents.PRIVATE_MESSAGE, (message) =>
+      privateMessages(message)
+    );
+  } catch (error) {
+    socket.emit(socketEvents.SERVER_ERROR, error.message);
+  }
 }
--- server/src/tasks/directMessage.js
+++ server/src/tasks/directMessage.js
@@ -1,31 +1,29 @@
 import { RedisMessageStorage } from "../services/message.js";
 import { RedisSessionStorage } from "../services/session.js";
 import { io, socket } from "../../socket.js";
+import { decodeToken } from "../config/jwt.js";
 import socketEvents from "../config/socketEvents.js";
 
 const messageStorage = new RedisMessageStorage();
 const sessionStorage = new RedisSessionStorage();
 
 export async function userMessages({ userId, username }) {
-  const [session, userMessages, connectionSocket] = await Promise.all([
-    getLastSeen(userId),
-    getMessagesForUser(socket.userId),
-    io.in(userId).allSockets(),
-  ]);
-  socket.emit(socketEvents.USER_MESSAGES, {
-    userId,
-    username,
-    messages: userMessages.get(userId) || [],
-    connected: connectionSocket.size === 0 ? false : true,
-    lastSeen: session?.lastSeen != null ? new Date(session.lastSeen) : null,
-  });
-}
-
-export async function getLastSeen(userId) {
   try {
-    const lastSeen = await sessionStorage.getLastSeen(userId);
-    if (lastSeen.result) return lastSeen.data;
-    else throw lastSeen.error;
+    const isValid = decodeToken(socket.token);
+    if (isValid) {
+      const [session, userMessages, connectionSocket] = await Promise.all([
+        getLastSeen(userId),
+        getMessagesForUser(socket.userId),
+        io.in(userId).allSockets(),
+      ]);
+      socket.emit(socketEvents.USER_MESSAGES, {
+        userId,
+        username,
+        messages: userMessages.get(userId) || [],
+        connected: connectionSocket.size === 0 ? false : true,
+        lastSeen: session?.lastSeen != null ? new Date(session.lastSeen) : null,
+      });
+    }
   } catch (error) {
     throw error;
   }
@@ -40,6 +38,16 @@ export async function privateMessages({ content, to }) {
   };
   socket.to(to).emit(socketEvents.PRIVATE_MESSAGE, message);
   await messageStorage.saveMessage(message);
+}
+
+export async function getLastSeen(userId) {
+  try {
+    const lastSeen = await sessionStorage.getLastSeen(userId);
+    if (lastSeen.result) return lastSeen.data;
+    else throw lastSeen.error;
+  } catch (error) {
+    throw error;
+  }
 }
 
 async function getMessagesForUser(userId) {
