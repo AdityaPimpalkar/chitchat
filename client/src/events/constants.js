4ae37d9a64c59d11c4bf581e1fe1f8a2de15707b
Aditya Pimpalkar <adipimpalkar@gmail.com>
2022-02-15 23:48:04 +0530

    socket event constant

--- client/src/events/constants.js
+++ client/src/events/constants.js
@@ -5,6 +5,8 @@ const SocketEvents = {
   SERVER_ERROR: "server_error",
   SESSION: "session",
   LOAD_DATA: "load_data",
+  SEARCH_FRIEND: "search_friend",
+  ADD_FRIEND: "add_friend",
   USER_CONNECTED: "user_connected",
   USER_DISCONNECTED: "user_disconnected",
   PRIVATE_MESSAGE: "private_message",
