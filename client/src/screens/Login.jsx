d3685a2c610cc1743eed5156d5befd3e1b2dcf3b
Aditya Pimpalkar <adipimpalkar@gmail.com>
2022-03-06 22:29:18 +0530

    cleanup and refactoring

--- client/src/screens/Login.jsx
+++ client/src/screens/Login.jsx
@@ -1,22 +1,14 @@
 import React from "react";
 import GoogleLogin from "react-google-login";
 import config from "../config";
-
+import { ChatAlt2Icon } from "@heroicons/react/solid";
 const Login = ({ onSuccess, onFailure, isLoading, LoadWidth }) => {
   return (
     <div className="hidden md:hidden lg:block xl:block">
       <div className="w-full h-screen flex items-center justify-center">
         <form className="w-full md:w-1/3 bg-purple-900 rounded-xl border-2 shadow-xl">
           <div className="flex font-bold justify-center mt-6">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="text-white lg:h-16 w-16 mx-2 xl:h-18 w-18"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-            >
-              <path d="M2 5a2 2 0 012-2h7a2 2 0 012 2v4a2 2 0 01-2 2H9l-3 3v-3H4a2 2 0 01-2-2V5z" />
-              <path d="M15 7v2a4 4 0 01-4 4H9.828l-1.766 1.767c.28.149.599.233.938.233h2l3 3v-3h2a2 2 0 002-2V9a2 2 0 00-2-2h-1z" />
-            </svg>
+            <ChatAlt2Icon className="text-white lg:h-16 w-16 mx-2 xl:h-18 w-18" />
           </div>
           <h2 className="text-3xl text-center text-white mb-4">chitchat.io</h2>
           <div className="px-8 pb-10">
@@ -66,35 +58,6 @@ const Login = ({ onSuccess, onFailure, isLoading, LoadWidth }) => {
         </form>
       </div>
     </div>
-    // <div className="card w-100 text-center border-white">
-    //   <div className="row">
-    //     <div className="col-12">
-    //       <h5>Enter Username</h5>
-    //     </div>
-    //     <div className="d-flex justify-content-center py-1">
-    //       <div className="col-4">
-    //         <input
-    //           type="text"
-    //           name="username"
-    //           value={user}
-    //           className="form-control mb-3"
-    //           placeholder="Username"
-    //           autoComplete="off"
-    //           onChange={({ currentTarget: input }) => logNewUser(input.value)}
-    //           onKeyPress={(e) =>
-    //             e.code === "Enter" ? authenticateUser() : null
-    //           }
-    //         />
-    //         <button
-    //           className="btn btn-success w-100"
-    //           onClick={() => authenticateUser()}
-    //         >
-    //           Join!
-    //         </button>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </div>
   );
 };
 
